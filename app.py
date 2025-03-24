34ee67b5ba72c5d068ae7ef0ce16fc2a74c2814b
S12gb-2 <ndnha160603@gmail.com>
2025-03-24 23:15:08 +0700

    version 1

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def main():
             col1, col2 = st.columns(2)
             
             with col1:
-                st.image(image, caption="Uploaded Image", use_column_width=True)
+                st.image(image, caption="Uploaded Image", use_container_width=True)
             
             with col2:
                 with st.spinner("Generating caption..."):
@@ -214,7 +214,7 @@ def main():
             col1, col2 = st.columns(2)
             
             with col1:
-                st.image(image, caption="Uploaded Image", use_column_width=True)
+                st.image(image, caption="Uploaded Image", use_container_width=True)
             
             with col2:
                 with st.spinner("Classifying image..."):
